2d42a7d857832576fe760bcb05e445f7a178d2dc
ten-xv <kvaccaro@illinois.edu>
2016-11-10 14:44:20 -0600

    fix order likert

--- index-00-survey.php
+++ index-00-survey.php
@@ -64,101 +64,122 @@ echo '<!-- Survey -->
 
             <label class="statement">2. I enjoyed browsing this news feed</label>
                 <ul class="likert">
-                <li>
-                <input type="radio" name="likert-'; echo $page_id; echo '-2" value="strong_agree">
-                <label>Strongly agree</label>
+                                <li>
+                <input type="radio" name="likert-'; echo $page_id; echo '-2" value="strong_disagree">
+                <label>Strongly disagree</label>
                 </li>
-                <li>
-                <input type="radio" name="likert-'; echo $page_id; echo '-2" value="agree">
-                <label>Agree</label>
-                </li>
-                <li>
-                <input type="radio" name="likert-'; echo $page_id; echo '-2" value="some_agree">
-                <label>Somewhat agree</label>
-                </li>
-                <li>
-                <input type="radio" name="likert-'; echo $page_id; echo '-2" value="neutral">
-                <label>Neutral</label>
-                </li>
-                <li>
-                <input type="radio" name="likert-'; echo $page_id; echo '-2" value="some_disagree">
-                <label>Somewhat disagree</label>
-                </li>
+
                 <li>
                 <input type="radio" name="likert-'; echo $page_id; echo '-2" value="disagree">
                 <label>Disagree</label>
                 </li>
+
                 <li>
-                <input type="radio" name="likert-'; echo $page_id; echo '-2" value="strong_disagree">
-                <label>Strongly disagree</label>
+                <input type="radio" name="likert-'; echo $page_id; echo '-2" value="some_disagree">
+                <label>Somewhat disagree</label>
                 </li>
+
+                <li>
+                <input type="radio" name="likert-'; echo $page_id; echo '-2" value="neutral">
+                <label>Neutral</label>
+                </li>
+
+                                <li>
+                <input type="radio" name="likert-'; echo $page_id; echo '-2" value="some_agree">
+                <label>Somewhat agree</label>
+                </li>
+
+                 <li>
+                <input type="radio" name="likert-'; echo $page_id; echo '-2" value="agree">
+                <label>Agree</label>
+                </li>
+
+                <li>
+                <input type="radio" name="likert-'; echo $page_id; echo '-2" value="strong_agree">
+                <label>Strongly agree</label>
+                </li>
+
                 </ul>
 
 
             <label class="statement">3. I feel in control of my news feed</label>
                 <ul class="likert">
-                <li>
-                <input type="radio" name="likert-'; echo $page_id; echo '-3" value="strong_agree">
-                <label>Strongly agree</label>
+                                <li>
+                <input type="radio" name="likert-'; echo $page_id; echo '-3" value="strong_disagree">
+                <label>Strongly disagree</label>
                 </li>
-                <li>
-                <input type="radio" name="likert-'; echo $page_id; echo '-3" value="agree">
-                <label>Agree</label>
-                </li>
-                <li>
-                <input type="radio" name="likert-'; echo $page_id; echo '-3" value="some_agree">
-                <label>Somewhat agree</label>
-                </li>
-                <li>
-                <input type="radio" name="likert-'; echo $page_id; echo '-3" value="neutral">
-                <label>Neutral</label>
-                </li>
-                <li>
-                <input type="radio" name="likert-'; echo $page_id; echo '-3" value="some_disagree">
-                <label>Somewhat disagree</label>
-                </li>
+
                 <li>
                 <input type="radio" name="likert-'; echo $page_id; echo '-3" value="disagree">
                 <label>Disagree</label>
                 </li>
+
                 <li>
-                <input type="radio" name="likert-'; echo $page_id; echo '-3" value="strong_disagree">
-                <label>Strongly disagree</label>
+                <input type="radio" name="likert-'; echo $page_id; echo '-3" value="some_disagree">
+                <label>Somewhat disagree</label>
                 </li>
+
+                <li>
+                <input type="radio" name="likert-'; echo $page_id; echo '-3" value="neutral">
+                <label>Neutral</label>
+                </li>
+
+                                <li>
+                <input type="radio" name="likert-'; echo $page_id; echo '-3" value="some_agree">
+                <label>Somewhat agree</label>
+                </li>
+
+                 <li>
+                <input type="radio" name="likert-'; echo $page_id; echo '-3" value="agree">
+                <label>Agree</label>
+                </li>
+
+                <li>
+                <input type="radio" name="likert-'; echo $page_id; echo '-3" value="strong_agree">
+                <label>Strongly agree</label>
+                </li>
+
                 </ul>
 
 
 
             <label class="statement">4. I would like to use a control like this in my day-to-day browsing of Twitter</label>
                 <ul class="likert">
-                <li>
-                <input type="radio" name="likert-'; echo $page_id; echo '-4" value="strong_agree">
-                <label>Strongly agree</label>
+                                <li>
+                <input type="radio" name="likert-'; echo $page_id; echo '-4" value="strong_disagree">
+                <label>Strongly disagree</label>
                 </li>
-                <li>
-                <input type="radio" name="likert-'; echo $page_id; echo '-4" value="agree">
-                <label>Agree</label>
-                </li>
-                <li>
-                <input type="radio" name="likert-'; echo $page_id; echo '-4" value="some_agree">
-                <label>Somewhat agree</label>
-                </li>
-                <li>
-                <input type="radio" name="likert-'; echo $page_id; echo '-4" value="neutral">
-                <label>Neutral</label>
-                </li>
-                <li>
-                <input type="radio" name="likert-'; echo $page_id; echo '-4" value="some_disagree">
-                <label>Somewhat disagree</label>
-                </li>
+
                 <li>
                 <input type="radio" name="likert-'; echo $page_id; echo '-4" value="disagree">
                 <label>Disagree</label>
                 </li>
+
                 <li>
-                <input type="radio" name="likert-'; echo $page_id; echo '-4" value="strong_disagree">
-                <label>Strongly disagree</label>
+                <input type="radio" name="likert-'; echo $page_id; echo '-4" value="some_disagree">
+                <label>Somewhat disagree</label>
                 </li>
+
+                <li>
+                <input type="radio" name="likert-'; echo $page_id; echo '-4" value="neutral">
+                <label>Neutral</label>
+                </li>
+
+                                <li>
+                <input type="radio" name="likert-'; echo $page_id; echo '-4" value="some_agree">
+                <label>Somewhat agree</label>
+                </li>
+
+                 <li>
+                <input type="radio" name="likert-'; echo $page_id; echo '-4" value="agree">
+                <label>Agree</label>
+                </li>
+
+                <li>
+                <input type="radio" name="likert-'; echo $page_id; echo '-4" value="strong_agree">
+                <label>Strongly agree</label>
+                </li>
+
                 </ul>
                 </form>
 
