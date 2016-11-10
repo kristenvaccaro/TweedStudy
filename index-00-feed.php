61f83f3172f72c6a217a82f8b50076fa02d46700
ten-xv <kvaccaro@illinois.edu>
2016-11-10 13:34:27 -0600

    clearer instrux on main pages (plus when no controls)

--- index-00-feed.php
+++ index-00-feed.php
@@ -7,39 +7,44 @@ echo '<!-- instructions -->
         <div class="col-md-2">
             <h3>Instructions</h3>
         </div>
-            
+
         <div class="col-md-4">
-            <h3><small>Imagine you are browsing this feed on a Sunday morning, while drinking coffee (or other leisurely browsing time). Please browse for 10-20 seconds as you would in that situation.</small></h3>
+            <h3><small>Imagine you are browsing this feed on a Sunday morning, while drinking coffee (or a similar leisurely browsing time). If controls are available, please use them while browsing your feed to make your ideal feed for leisurely browsing. Please browse for 30-60 seconds as you would in that situation.</small></h3>
         </div>';
-    
+
 
         if ($value2 === "real" or $value2 === "random") {
-    
-        echo '<!-- controls constantly show atm-->
+
+        echo '<!-- controls -->
         <div class="col-md-6">
-            <h3><small>Controls are available for this news feed. Please use them for 10-20 seconds to make your ideal feed for a Sunday morning drinking coffee (or other leisurely browsing time)."</small></h3>
+            <h3><small>Controls are available for this news feed. Please use them while browsing to make your ideal feed for leisurely browsing."</small></h3>
         </div>';
+        } else { echo '<!-- no controls -->
+        <div class="col-md-6">
+            <h3><small>No controls are available for this news feed. This area intentionally left blank."</small></h3>
+        </div>'
+
         }
 
 echo '<!--end of the row-->
     </div>
-    
+
 </div>
 <!-- /instructions -->
-    
+
 
 <!-- feed+controls -->
 <div class="feed+controls">
     <div class="row">
         <div class="col-md-6">
             <a href="#"> </a>';
-    
+
                 if ($value2 === "real" or $value2 === "none") {
                     echo '<div id="feed-'.$page_id.'">';
                     printTweets_SQL_short();
                     echo '</div>';
                 }
-    
+
                 if ($value2 === "random") {
                     echo '<div id="feed2-'.$page_id.'">';
                     printTweets_SQL_rand_short();
@@ -48,7 +53,7 @@ echo '<!--end of the row-->
 
             echo '<!-- end of col-md-6 div-->
         </div>';
-    
+
 
         if ($value2 === "real" or $value2 === "random") {
         echo '<!-- controls constantly show atm-->
@@ -57,20 +62,20 @@ echo '<!--end of the row-->
                 <h3>Controls</h3>
                 <h4 class="controltype"></h4>
                 <div id="controlcontent">';
-    
+
                     include('index-00-controls.php');
-            
+
                 echo '<!-- end of controls -->
                 </div>
             </div>
         </div>';
         }
-    
+
     echo '<!--end of row-->
     </div>
 
 </div>
 <!-- /feed+controls -->
-    
+
 <hr>';
 ?>
