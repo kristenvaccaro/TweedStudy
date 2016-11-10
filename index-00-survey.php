fa14709739746afaa7bc992d469d63893031d0ae
ten-xv <kvaccaro@illinois.edu>
2016-11-10 14:42:05 -0600

    fix order likert

--- index-00-survey.php
+++ index-00-survey.php
@@ -5,52 +5,63 @@
 
 
 <?php
- 
+
 echo '<!-- Survey -->
 <div id="surveysection" class="row">
     <div class="col-md-5">
         <h3>Instructions</h3>
-    
+
         <h3><small>Please answer the following questions based on your experience with this news feed.</small></h3>
     </div>
-    
+
     <div class="col-md-7">
         <h3>Survey</h3>
-    
+
         <div class="survey">
             <form action="">
             <label class="statement">1. I am satisfied with the final news feed I saw for leisurely browsing</label>
                 <ul class="likert">
-                <li>
-                <input type="radio" name="likert-'; echo $page_id; echo '-1" value="strong_agree">
-                <label>Strongly agree</label>
-                </li>
-                <li>
-                <input type="radio" name="likert-'; echo $page_id; echo '-1" value="agree">
-                <label>Agree</label>
-                </li>
-                <li>
-                <input type="radio" name="likert-'; echo $page_id; echo '-1" value="some_agree">
-                <label>Somewhat agree</label>
-                </li>
-                <li>
-                <input type="radio" name="likert-'; echo $page_id; echo '-1" value="neutral">
-                <label>Neutral</label>
-                </li>
-                <li>
-                <input type="radio" name="likert-'; echo $page_id; echo '-1" value="some_disagree">
-                <label>Somewhat disagree</label>
-                </li>
-                <li>
-                <input type="radio" name="likert-'; echo $page_id; echo '-1" value="disagree">
-                <label>Disagree</label>
-                </li>
+
+
                 <li>
                 <input type="radio" name="likert-'; echo $page_id; echo '-1" value="strong_disagree">
                 <label>Strongly disagree</label>
                 </li>
+
+                <li>
+                <input type="radio" name="likert-'; echo $page_id; echo '-1" value="disagree">
+                <label>Disagree</label>
+                </li>
+
+                <li>
+                <input type="radio" name="likert-'; echo $page_id; echo '-1" value="some_disagree">
+                <label>Somewhat disagree</label>
+                </li>
+
+                <li>
+                <input type="radio" name="likert-'; echo $page_id; echo '-1" value="neutral">
+                <label>Neutral</label>
+                </li>
+
+                                <li>
+                <input type="radio" name="likert-'; echo $page_id; echo '-1" value="some_agree">
+                <label>Somewhat agree</label>
+                </li>
+
+                 <li>
+                <input type="radio" name="likert-'; echo $page_id; echo '-1" value="agree">
+                <label>Agree</label>
+                </li>
+
+                <li>
+                <input type="radio" name="likert-'; echo $page_id; echo '-1" value="strong_agree">
+                <label>Strongly agree</label>
+                </li>
+
+
+
                 </ul>
-            
+
             <label class="statement">2. I enjoyed browsing this news feed</label>
                 <ul class="likert">
                 <li>
@@ -82,8 +93,8 @@ echo '<!-- Survey -->
                 <label>Strongly disagree</label>
                 </li>
                 </ul>
-            
-            
+
+
             <label class="statement">3. I feel in control of my news feed</label>
                 <ul class="likert">
                 <li>
@@ -115,9 +126,9 @@ echo '<!-- Survey -->
                 <label>Strongly disagree</label>
                 </li>
                 </ul>
-            
-            
-            
+
+
+
             <label class="statement">4. I would like to use a control like this in my day-to-day browsing of Twitter</label>
                 <ul class="likert">
                 <li>
@@ -150,9 +161,9 @@ echo '<!-- Survey -->
                 </li>
                 </ul>
                 </form>
-        
+
         </div>
     </div>
 </div>';
-    
+
 ?>
