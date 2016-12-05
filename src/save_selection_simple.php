ed15ad0b889993af61403cf741aabdfb0d39f7a8
ten-xv <kvaccaro@illinois.edu>
2016-12-05 14:02:19 -0600

    maybe data not accessible, instead try response?

--- src/save_selection_simple.php
+++ src/save_selection_simple.php
@@ -30,4 +30,29 @@
 //    echo $dataString;
 
 	// printTweets_SQL_short();
+
+$servername = "engr-cpanel-mysql.engr.illinois.edu";
+      $username = "twitterf_user";
+      $password = "IIA@kT$7maLt";
+      $dbname = "twitterf_tweet_store";
+
+      $db = new mysqli($servername, $username, $password, $dbname);
+
+      if($db->connect_errno > 0){
+          die('Unable to connect to database [' . $db->connect_error . ']');
+      }
+
+      $user_id = $_SESSION["user_id"];
+
+
+      $sql = "INSERT INTO survey_responses (user_id, page, test) VALUES (?,?,1)";
+
+      if(!$result = $db->query($sql)){
+          die('There was an error running the query [' . $db->error . ']');
+      }
+
+
+       $db->close();
+
+
 ?>
