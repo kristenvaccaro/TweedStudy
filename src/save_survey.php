4cbbe26a3adef1fafbb559897ea33b3ecc3fca98
ten-xv <kvaccaro@illinois.edu>
2016-12-05 16:21:16 -0600

    maybe data not accessible, instead try response?

--- src/save_survey.php
+++ src/save_survey.php
@@ -39,15 +39,15 @@
 
     // $stmt = $db->prepare("INSERT INTO survey_responses (user_id, page, controltype, realrandnone, question1, question2, question3, question4) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
 
-    $stmt = $db->prepare("REPLACE INTO survey_responses (user_id, page, controltype, realrandnone, question1, question2, question3, question4) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt = $db->prepare("INSERT INTO survey_responses (user_id, page, controltype, realrandnone, question1, question2, question3, question4) VALUES (?, ?, ?, ?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE controltype = ?, realrandnone = ?, question1 = ?, question2 = ?, question3 = ?, question4 = ? ");
 
     if ( false===$stmt ) {
         die('prepare() failed: ' . htmlspecialchars($mysqli->error));
     }
 
-    $stmt->bind_param("ssssssss", $user_id, $page, $controltype, $rrn, $q1,$q2,$q3,$q4);
+    $stmt->bind_param("ssssssssssssss", $user_id, $page, $controltype, $rrn, $q1,$q2,$q3,$q4,$controltype, $rrn, $q1,$q2,$q3,$q4);
 
-    $rc2 = $stmt->bind_param("ssssssss",  $user_id, $page, $controltype, $rrn, $q1,$q2,$q3,$q4);
+    $rc2 = $stmt->bind_param("ssssssssssssss",  $user_id, $page, $controltype, $rrn, $q1,$q2,$q3,$q4,$controltype, $rrn, $q1,$q2,$q3,$q4);
 
     if ( false===$rc2 ) {
         // again execute() is useless if you can't bind the parameters. Bail out somehow.
