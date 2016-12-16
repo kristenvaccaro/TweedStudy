3633f4e46ce2d7221160d1a158732694add51cf0
ten-xv <kvaccaro@illinois.edu>
2016-12-16 08:51:43 -0600

    nav bar at top too

--- index-00.php
+++ index-00.php
@@ -105,7 +105,7 @@ include 'src/authorization.php';
 <div class="col-lg-12">
 <h1 class="page-header" style="display:inline;">Twitter News Feed Study
 </h1> <nav>
-<ul class="pagination">
+<ul class="pagination" style ="display:inline">
 
 <?php
     $pages=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19];
