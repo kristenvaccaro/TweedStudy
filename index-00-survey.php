acff64b131dad0c52bef38872fcb3304c291e580
ten-xv <kvaccaro@illinois.edu>
2016-12-20 10:56:40 -0600

    minor edits to text of survey

--- index-00-survey.php
+++ index-00-survey.php
@@ -12,7 +12,7 @@ echo '<!-- Survey -->
     <div class="col-md-5">
         <h3 style="margin-left:15px;">Instructions</h3>
 
-        <h3 style="margin-left:15px;><small>Please answer the following questions based on your experience with this news feed.</small></h3>
+        <h3 style="margin-left:15px;"><small>Please answer the following questions based on your experience with this news feed.</small></h3>
     </div>
 
     <div class="col-md-7">
