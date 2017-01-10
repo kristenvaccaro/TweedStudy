2b77c8be81c9ad42a9a2f37a9c0dc9277546a39a
ten-xv <kvaccaro@illinois.edu>
2017-01-10 16:43:28 -0600

    fixing instructions

--- index-00-feed.php
+++ index-00-feed.php
@@ -9,7 +9,7 @@ echo '<!-- instructions -->
         </div> -->
 
         <div class="col-md-7">
-            <h3>Imagine you are browsing this feed on a Sunday morning, while drinking coffee (or a similar leisurely browsing time). Please browse for 30-60 seconds as you would in that situation.</h3>
+            <h4 style="margin-left: 14px;  line-height: 1.5;">Imagine you are browsing this feed on a Sunday morning, while drinking coffee (or a similar leisurely browsing time). Please browse for 30-60 seconds as you would in that situation.</h4>
         </div>';
 
 
@@ -17,7 +17,7 @@ echo '<!-- instructions -->
 
         echo '<!-- controls constantly show atm-->
         <div class="col-md-5">
-            <h3 style="margin-left: 15px; margin-right: 15px;">Controls are available for this news feed. Please use them for 10-20 seconds to make your ideal feed for a leisurely browsing time (e.g. a Sunday morning drinking coffee).</h3>
+            <h4 style="margin-left: 15px; margin-right: 15px; line-height: 1.5;">Controls are available for this news feed. Please use them for 10-20 seconds to make your ideal feed for a leisurely browsing time (e.g. a Sunday morning drinking coffee).</h4>
         </div>';
         }else{ echo '<!-- no controls -->
         <div class="col-md-5">
