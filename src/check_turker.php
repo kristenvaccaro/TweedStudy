56d85f6e2a87335e59e03c1a09eb89cd130b726d
ten-xv <kvaccaro@illinois.edu>
2017-01-17 14:56:36 -0600

    debug turk check

--- src/check_turker.php
+++ src/check_turker.php
@@ -1,27 +1,29 @@
 <?php
 
 //This is to check whether the submitted TurkerID already existed in database
-include_once('webpage-utility/db_utility.php');
-$conn = connect_to_db();
+// include_once('webpage-utility/db_utility.php');
+// $conn = connect_to_db();
 
-if($stmt = $conn->prepare('SELECT FeedbackID FROM Feedback WHERE turkerID = ?')){
+// if($stmt = $conn->prepare('SELECT FeedbackID FROM Feedback WHERE turkerID = ?')){
 
-	$stmt->bind_param('s', $_POST['turker']);
-}
+// 	$stmt->bind_param('s', $_POST['turker']);
+// }
 
-$stmt->execute();
-$res = $stmt->get_result();
+// $stmt->execute();
+// $res = $stmt->get_result();
 
-if(mysqli_num_rows($res) > 0){
-	echo "exists";
-	// $result = "exists";
-}
-else{
-	echo "success";
-	// $result = "success";
-}
+// if(mysqli_num_rows($res) > 0){
+// 	echo "exists";
+// 	// $result = "exists";
+// }
+// else{
+// 	echo "success";
+// 	// $result = "success";
+// }
 
 
-mysqli_close($conn);
+// mysqli_close($conn);
+
+echo "exists";
 
 ?>
