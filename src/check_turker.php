8d531cf5d857a94fd45109be75d9e32086d627df
ten-xv <kvaccaro@illinois.edu>
2017-01-17 16:56:45 -0600

    reformat turker check for my db format

--- src/check_turker.php
+++ src/check_turker.php
@@ -1,36 +1,38 @@
 <?php
 
+session_start();
+
 $tid = $_POST['turker'];
 
-// $_SESSION['turker_id'] =  $tid;
+$_SESSION['turker_id'] =  $tid;
 
-// $servername = "engr-cpanel-mysql.engr.illinois.edu";
-// $username = "twitterf_user";
-// $password = "IIA@kT$7maLt";
-// $dbname = "twitterf_tweet_store";
+$servername = "engr-cpanel-mysql.engr.illinois.edu";
+$username = "twitterf_user";
+$password = "IIA@kT$7maLt";
+$dbname = "twitterf_tweet_store";
 
-// $db = new mysqli($servername, $username, $password, $dbname);
+$db = new mysqli($servername, $username, $password, $dbname);
 
-// if($db->connect_errno > 0){
-//     die('Unable to connect to database [' . $db->connect_error . ']');
-// }
+if($db->connect_errno > 0){
+    die('Unable to connect to database [' . $db->connect_error . ']');
+}
 
-// $uid = $_SESSION["user_id"];
+$uid = $_SESSION["user_id"];
 
-// $sql = "SELECT user_id FROM `survey_responses_userinfo` WHERE turkerID = {$tid}";
+$sql = "SELECT user_id FROM `survey_responses_userinfo` WHERE turkerID = {$tid}";
 
-// if(!$result = $db->query($sql)){
-//         die('There was an error running the query [' . $db->error . ']');
-// }
+if(!$result = $db->query($sql)){
+        die('There was an error running the query [' . $db->error . ']');
+}
 
-// if($result->num_rows === 0)
-//     {
-//         echo "success";
-//     }
-//     else
-//     {
-//         echo "exists";
-//     }
+if($result->num_rows === 0)
+    {
+        echo "success";
+    }
+    else
+    {
+        echo "exists";
+    }
 
 // echo "success";
 
