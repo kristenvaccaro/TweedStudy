d54448cfb784260413048508ef23322a688c5cd9
ten-xv <kvaccaro@illinois.edu>
2017-01-18 12:46:10 -0600

    reformat turker check for my db format

--- src/check_turker.php
+++ src/check_turker.php
@@ -4,40 +4,42 @@ session_start();
 
 $tid = $_POST['turker'];
 
-$_SESSION['turker_id'] =  $tid;
+echo $tid;
 
-$servername = "engr-cpanel-mysql.engr.illinois.edu";
-$username = "twitterf_user";
-$password = "IIA@kT$7maLt";
-$dbname = "twitterf_tweet_store";
+// $_SESSION['turker_id'] =  $tid;
 
-$db = new mysqli($servername, $username, $password, $dbname);
+// $servername = "engr-cpanel-mysql.engr.illinois.edu";
+// $username = "twitterf_user";
+// $password = "IIA@kT$7maLt";
+// $dbname = "twitterf_tweet_store";
 
-if($db->connect_errno > 0){
-    die('Unable to connect to database [' . $db->connect_error . ']');
-}
+// $db = new mysqli($servername, $username, $password, $dbname);
 
-$uid = $_SESSION["user_id"];
+// if($db->connect_errno > 0){
+//     die('Unable to connect to database [' . $db->connect_error . ']');
+// }
 
-$sql = "SELECT user_id FROM `survey_responses_userinfo`";
+// $uid = $_SESSION["user_id"];
 
-// $sql = "SELECT user_id FROM `survey_responses_userinfo` WHERE turkerID = " . $tid;
+// $sql = "SELECT user_id FROM `survey_responses_userinfo`";
+
+// // $sql = "SELECT user_id FROM `survey_responses_userinfo` WHERE turkerID = " . $tid;
 
 
-// $sql = "SELECT user_id FROM `survey_responses_userinfo` WHERE turkerID = {$tid}";
+// // $sql = "SELECT user_id FROM `survey_responses_userinfo` WHERE turkerID = {$tid}";
 
-if(!$result = $db->query($sql)){
-        die('There was an error running the query [' . $db->error . ']');
-}
+// if(!$result = $db->query($sql)){
+//         die('There was an error running the query [' . $db->error . ']');
+// }
 
-if($result->num_rows === 0)
-    {
-        echo "success";
-    }
-    else
-    {
-        echo "exists";
-    }
+// if($result->num_rows === 0)
+//     {
+//         echo "success";
+//     }
+//     else
+//     {
+//         echo "exists";
+//     }
 
 // echo "success";
 
