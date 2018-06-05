7e7680c5dca34510514081388c86ec79746ebb1b
ten-xv <kvaccaro@illinois.edu>
2018-06-05 16:06:32 -0500

    servername change made things worse

--- src/saveToSQL.php
+++ src/saveToSQL.php
@@ -1,6 +1,6 @@
 <?php
     function saveToSQL($connection,$max_id) {
-        $servername = "engr-cpanel3-mysql.engr.illinois.edu";
+        $servername = "engr-cpanel-mysql.engr.illinois.edu";
         $username = "twitterf_user";
         $password = "IIA@kT$7maLt";
         $dbname = "twitterf_tweet_store";
