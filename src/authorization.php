6db8ab99339c5133f64532143e271081d604d993
ten-xv <kvaccaro@illinois.edu>
2018-06-08 15:46:44 -0500

    try unset but not local?

--- src/authorization.php
+++ src/authorization.php
@@ -37,7 +37,6 @@
                                echo "<br> end of request token printing <br>";
         $_SESSION['oauth_access_token'] = $request_token['oauth_token'];
         $_SESSION['oauth_access_token_secret'] = $request_token['oauth_token_secret'];
-        unset($connection);
     } else {
         //                        echo $_SESSION['oauth_access_token'];
         //                        echo "<br>";
@@ -48,7 +47,7 @@
     //                    $request_token['oauth_token'] = $_SESSION['oauth_token'];
     //                    $request_token['oauth_token_secret'] = $_SESSION['oauth_token_secret'];
 
-
+    unset($connection);
     $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $_SESSION['oauth_access_token'], $_SESSION['oauth_access_token_secret']);
     $access_token = $connection->oauth("oauth/access_token", ["oauth_verifier" => $_REQUEST['oauth_verifier']]);
     $_SESSION['access_token'] = $access_token;
